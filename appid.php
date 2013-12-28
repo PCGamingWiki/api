5d5b86b0e45ac94b1a43f24f70efa6a6b617e7ae
Stanisław Gackowski <soeb@soeb.eu>
2013-12-28 02:08:39 +0100

    Tabs.

--- appid.php
+++ appid.php
@@ -11,45 +11,45 @@
 
 // Config
 $config = array(
-        'api_url' => 'http://pcgamingwiki.com/wiki/Special:Ask/-5B-5BSteam-20AppID::%APPID%-5D-5D/format%3Djson', // Format of URL to get APPID data as json.
+	'api_url' => 'http://pcgamingwiki.com/wiki/Special:Ask/-5B-5BSteam-20AppID::%APPID%-5D-5D/format%3Djson', // Format of URL to get APPID data as json.
 );
 
 
 // Main code
 if (!isset($_GET['appid'])) {
-        die("Error: No appid provided.");
+	die("Error: No appid provided.");
 }
 else {
-        $appid = $_GET['appid'];
+	$appid = $_GET['appid'];
 
-        // Construct api_url for appid
-        $url = str_replace('%APPID%', $appid, $config['api_url']);
+	// Construct api_url for appid
+	$url = str_replace('%APPID%', $appid, $config['api_url']);
 
-        // Fetch data
-        $hcurl = curl_init( $url );
-        curl_setopt( $hcurl, CURLOPT_RETURNTRANSFER, 1 );
-        curl_setopt( $hcurl, CURLOPT_TIMEOUT, 5 );
-        curl_setopt( $hcurl, CURLOPT_CONNECTTIMEOUT, 5 );
+	// Fetch data
+	$hcurl = curl_init( $url );
+	curl_setopt( $hcurl, CURLOPT_RETURNTRANSFER, 1 );
+	curl_setopt( $hcurl, CURLOPT_TIMEOUT, 5 );
+	curl_setopt( $hcurl, CURLOPT_CONNECTTIMEOUT, 5 );
 
-        $data = curl_exec( $hcurl );
-        curl_close( $hcurl );
+	$data = curl_exec( $hcurl );
+	curl_close( $hcurl );
 
-        // Read data as JSON
-        $json = json_decode( $data, true );
-        if (!$json) {
-                die("Error: Received invalid data from Semantic MediaWiki.");
-        }
+	// Read data as JSON
+	$json = json_decode( $data, true );
+	if (!$json) {
+		die("Error: Received invalid data from Semantic MediaWiki.");
+	}
 
-        $results = $json['results'];
-        if (count($results) == 0) {
-                die("Error: No results from Semantic MediaWiki.");
-        }
+	$results = $json['results'];
+	if (count($results) == 0) {
+		die("Error: No results from Semantic MediaWiki.");
+	}
 
-        // Get first results url
-        $first_result = reset($results);
-        $article_url = $first_result["fullurl"];
+	// Get first results url
+	$first_result = reset($results);
+	$article_url = $first_result["fullurl"];
 
-        // Redirect pls valve
-        header('Location: ' . $article_url);
-        die();
+	// Redirect pls valve
+	header('Location: ' . $article_url);
+	die();
 }
